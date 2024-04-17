4197a5385a8c5390ba1b20e438e6d05dd7571b78
NathaliaFon <nasantanafonseca@gamil.com>
2024-04-17 20:12:08 -0300

    0.0.0

---
+++ src/main/java/com/itau/registeringppms/RegisteringPpMsApplication.java
@@ -0,0 +1,13 @@
+package com.itau.registeringppms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisteringPpMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisteringPpMsApplication.class, args);
+	}
+
+}
